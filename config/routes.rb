b9ad0a983e91b2f2f114721645e4eef7883a5fcb
sydneygold <sydney.goldberg@outlook.com>
2020-12-03 13:14:27 -0700

    added destroy and create routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  resources :friends, only: [:index, :show, :update, :delete]
+  resources :friends, only: [:index, :show, :update, :destroy, :create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
